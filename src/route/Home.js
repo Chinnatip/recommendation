9f4b3f094c580fab75903e1b1684440379851228
Chinnatip Taemkaeo <chinnatip@me.com>
2018-12-05 19:52:07 +0700

    styled-component

--- src/route/Home.js
+++ src/route/Home.js
@@ -1,7 +1,18 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
+import { Button } from 'antd'
 
 const Home = () => {
-  return <h1>This is Home Page</h1>
+  return (
+    <Fragment>
+      <h1>This is Home Page</h1>
+      <div>
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
+      </div>
+    </Fragment>
+  )
 }
 
 export default Home
